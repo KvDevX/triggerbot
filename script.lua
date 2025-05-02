10890a3f0f149e986f6de52fdfbe3c1693698b47
KvDevX <kvdex9258@gmail.com>
2025-05-01 22:58:36 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -103,7 +103,7 @@ getgenv().ExunysDeveloperAimbot = {
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
 		LockPart = "Head", -- Body part to lock on
 
-		TriggerKey = Enum.UserInputType.x,
+		TriggerKey = Enum.KeyCode.X,
 		Toggle = false
 	},
 
